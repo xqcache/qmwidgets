e4cbddda35332e47a1b604d9f3373bbdbcff662c
xqcache <hicacheus@outlook.com>
2025-01-02 11:10:53 +0800

    增加资源初始化宏

--- src/qmwidgets_global.h
+++ src/qmwidgets_global.h
@@ -3,6 +3,7 @@
 #include <QtCore/qglobal.h>
 
 #ifndef BUILD_STATIC
+#define QMWIDGETS_INITIALIZE_RESOURCE
 #if defined(QMWIDGETS_COMPILE_LIB)
 #define QMWIDGETS_EXPORT Q_DECL_EXPORT
 #else
@@ -10,4 +11,21 @@
 #endif
 #else
 #define QMWIDGETS_EXPORT
+
+extern int qInitResources_qmassets();
+extern int qCleanupResources_qmassets();
+
+struct QmWidgetResourceInitializer {
+    QmWidgetResourceInitializer()
+    {
+        qInitResources_qmassets();
+    }
+    ~QmWidgetResourceInitializer()
+    {
+        qCleanupResources_qmassets();
+    }
+};
+
+#define QMWIDGETS_INITIALIZE_RESOURCE \
+    QmWidgetResourceInitializer __g_qmwidgets_resource_initializer__;
 #endif
