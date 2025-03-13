a3a0b7fc70dabaf66b6d1ff13985817c31ca5d1a
xqliang <hicacheus@outlook.com>
2025-03-13 10:14:15 +0800

    通过编译宏控制是否接管Win32 HTCAPTION事件

--- src/qmframelesswindow.cpp
+++ src/qmframelesswindow.cpp
@@ -1,4 +1,5 @@
 #include "qmframelesswindow.h"
+#include "qmwidgets_config.h"
 
 #if defined(WIN32) || defined(WIN64)
 // clang-format off
@@ -14,8 +15,18 @@ namespace {
 static constexpr int kResizeBorderWidth = 8;
 }
 
+struct QmFramelessWindowPrivate {
+    QWidget* title_bar { nullptr };
+
+#if (!defined(WIN32) || !defined(WIN64)) || !defined(TAKEOVER_WIN32_HTCAPTION_EVENT)
+    QPoint mouse_pos;
+    bool mouse_pressed { false };
+#endif
+};
+
 QmFramelessWindow::QmFramelessWindow(QWidget* parent)
     : QMainWindow(parent)
+    , d_(new QmFramelessWindowPrivate)
 {
     setWindowFlags(
         windowFlags() | Qt::WindowType::Window | Qt::WindowSystemMenuHint | Qt::WindowMinimizeButtonHint | Qt::WindowMaximizeButtonHint);
@@ -28,6 +39,33 @@ QmFramelessWindow::QmFramelessWindow(QWidget* parent)
     // 必须保留一个像素的边框，不然Windows不会绘制窗口阴影
     const MARGINS shadow = { 1, 1, 1, 1 };
     ::DwmExtendFrameIntoClientArea(hwnd, &shadow);
+#endif
+}
+
+QmFramelessWindow::~QmFramelessWindow() noexcept
+{
+    delete d_;
+}
+
+void QmFramelessWindow::setTitleBar(QWidget* widget)
+{
+    if (!widget) {
+        return;
+    }
+    if (d_->title_bar) {
+#if (!defined(WIN32) || !defined(WIN64)) || !defined(TAKEOVER_WIN32_HTCAPTION_EVENT)
+        d_->title_bar->removeEventFilter(this);
+#endif
+        removeEventFilter(d_->title_bar);
+    }
+
+    d_->title_bar = widget;
+    connect(d_->title_bar, &QObject::destroyed, this, [this] { d_->title_bar = nullptr; });
+    setMenuWidget(d_->title_bar);
+    installEventFilter(d_->title_bar);
+
+#if (!defined(WIN32) || !defined(WIN64)) || !defined(TAKEOVER_WIN32_HTCAPTION_EVENT)
+    d_->title_bar->installEventFilter(this);
 #endif
 }
 
@@ -49,24 +87,24 @@ bool QmFramelessWindow::nativeEvent(const QByteArray& eventType, void* message, 
         if (0 != *result) {
             return true;
         }
+
+#if defined(TAKEOVER_WIN32_HTCAPTION_EVENT)
         QWidget* menu_widget = menuWidget();
-        if (!menu_widget) {
-            return false;
+        if (menu_widget) {
+            // support highdpi
+            double dpr = devicePixelRatioF();
+            QPoint pos = menu_widget->mapFromGlobal(QPoint(x / dpr, y / dpr));
+            if (menu_widget->rect().contains(pos)) {
+                // 判断当前是否点击在了MenuWidget的子控件上了，如果点在空白区域，就支持标题栏移动
+                QWidget* child = menu_widget->childAt(pos);
+                if (!child) {
+                    *result = HTCAPTION;
+                    return true;
+                }
+            }
         }
-        // support highdpi
-        double dpr = devicePixelRatioF();
-        QPoint pos = menu_widget->mapFromGlobal(QPoint(x / dpr, y / dpr));
-        if (!menu_widget->rect().contains(pos)) {
-            return false;
-        }
-        // 判断当前是否点击在了MenuWidget的子控件上了，如果点在空白区域，就支持标题栏移动
-        QWidget* child = menu_widget->childAt(pos);
-        if (!child) {
-            *result = HTCAPTION;
-            return true;
-        }
-        return false;
-    }
+#endif
+    } break;
     default:
         return QMainWindow::nativeEvent(eventType, message, result);
     }
@@ -95,6 +133,52 @@ bool QmFramelessWindow::event(QEvent* event)
     }
 #endif
     return QMainWindow::event(event);
+}
+
+bool QmFramelessWindow::eventFilter(QObject* watched, QEvent* event)
+{
+    if (watched != d_->title_bar) {
+        return QMainWindow::eventFilter(watched, event);
+    }
+
+#if (!defined(WIN32) || !defined(WIN64)) || !defined(TAKEOVER_WIN32_HTCAPTION_EVENT)
+    switch (event->type()) {
+    case QEvent::MouseButtonDblClick:
+        if (window()->isMaximized()) {
+            window()->showNormal();
+        } else {
+            window()->showMaximized();
+        }
+        return true;
+    case QEvent::MouseButtonPress:
+        if (static_cast<QMouseEvent*>(event)->button() == Qt::LeftButton) {
+            d_->mouse_pos = static_cast<QMouseEvent*>(event)->pos();
+            d_->mouse_pressed = true;
+            return true;
+        }
+        break;
+    case QEvent::MouseMove:
+        if (d_->mouse_pressed && !window()->isMaximized()) {
+            window()->move(window()->pos() + static_cast<QMouseEvent*>(event)->pos() - d_->mouse_pos);
+            return true;
+        }
+        break;
+    case QEvent::MouseButtonRelease:
+        if (d_->mouse_pressed) {
+            d_->mouse_pressed = false;
+            return true;
+        }
+        break;
+    default:
+        break;
+    }
+#endif
+    return QMainWindow::eventFilter(watched, event);
+}
+
+QWidget* QmFramelessWindow::titleBar() const
+{
+    return d_->title_bar;
 }
 
 #if defined(WIN32) || defined(WIN64)
--- src/qmframelesswindow.h
+++ src/qmframelesswindow.h
@@ -3,14 +3,21 @@
 #include "qmwidgets_global.h"
 #include <QMainWindow>
 
+struct QmFramelessWindowPrivate;
+
 class QMWIDGETS_EXPORT QmFramelessWindow : public QMainWindow {
     Q_OBJECT
 public:
     explicit QmFramelessWindow(QWidget* parent = nullptr);
+    ~QmFramelessWindow() noexcept override;
+
+    void setTitleBar(QWidget* widget);
+    QWidget* titleBar() const;
 
 protected:
     bool nativeEvent(const QByteArray& eventType, void* message, qint64* result) override;
     bool event(QEvent* event) override;
+    bool eventFilter(QObject* watched, QEvent* event) override;
 
 private:
 #if defined(WIN32) || defined(WIN64)
@@ -18,5 +25,5 @@ private:
 #endif
 
 private:
-    bool m_isMaximized = false;
+    QmFramelessWindowPrivate* d_ { nullptr };
 };
